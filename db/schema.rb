2b5e493c5608c14021649fdf9065763695de4a20
Adam <electronicbattleweapon7@gmail.com>
2010-04-25 14:03:06 +0100

    apply db migration for products

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100422185426) do
+ActiveRecord::Schema.define(:version => 20100425130033) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(:version => 20100422185426) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "price",       :precision => 8, :scale => 2, :default => 0.0
   end
 
 end
