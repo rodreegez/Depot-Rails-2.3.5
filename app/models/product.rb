e6282dc08a79c3282d71141e228719b7c9a78308
Adam <electronicbattleweapon7@gmail.com>
2010-04-25 14:44:20 +0100

    validate unique title and that the image string looks like an image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
   validates_presence_of :title, :description, :image_url
   validates_numericality_of :price
+  validates_uniqueness_of :title
+  validates_format_of :image_url,
+                      :with => %r{\.(gif|jpg|png)$}i,
+                      :message => 'must be a URL for GIF, JPG ' + 'or PNG image.'
 
   validate :price_must_be_at_least_a_cent
 
