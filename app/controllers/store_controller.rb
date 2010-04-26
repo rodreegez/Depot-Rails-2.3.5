c6979f238aeb69a900cafb10a0d12b019afac611
Adam <electronicbattleweapon7@gmail.com>
2010-04-26 21:55:19 +0100

    remove The Flash and use our redirect_to_index method with no message

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -24,8 +24,7 @@ class StoreController < ApplicationController
 
   def empty_cart
     session[:cart] = nil
-    flash[:notice] = "Your cart is currently empty"
-    redirect_to :action => 'index'
+    redirect_to_index
   end
 
   private
