d5bc47162a01a50a7baad50b1c7763284b9ee3ee
Adam <electronicbattleweapon7@gmail.com>
2010-04-29 19:14:43 +0100

    create the save_order method

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,16 @@ class Order < ActiveRecord::Base
       ["Credit Card", "cc"],
       ["Purchace Order", "po"]
     ]
+
+    validates_presence_of :name, :address, :email, :pay_type
+    validates_inclusion_of :pay_type, :in =>
+                            PAYMENT_TYPES.map {|disp, value| value}
+
+    def add_line_items_from_cart(cart)
+      cart.items.each do |i|
+        li = LineItem.from_cart_item(i)
+        line_items << li
+      end
+    end
+
 end
