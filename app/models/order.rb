0ac0d4287002430a39adbf6ba2589938c28e612b
Adam <electronicbattleweapon7@gmail.com>
2010-05-06 19:09:49 +0100

    formatting

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,8 @@ class Order < ActiveRecord::Base
   has_many :line_items
 
   PAYMENT_TYPES = [
-      ["Check", "check"],
-      ["Credit Card", "cc"],
+      ["Check",       "check"],
+      ["Credit Card",    "cc"],
       ["Purchace Order", "po"]
     ]
 
