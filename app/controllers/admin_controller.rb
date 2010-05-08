84974b6728283b332c1b2a81f6ce9f1631a1acf2
Adam <electronicbattleweapon7@gmail.com>
2010-05-08 13:30:45 +0100

    postback handeling to determine if to display the form or the index

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,14 @@
 class AdminController < ApplicationController
   def login
+    if request.post?
+      user = User.authenticate(params[:name], params[:password])
+      if user
+        session[:user_id] = user.id
+        redirect_to(:action => "index")
+      else
+        flash.now[:notice] = "I'm sorry, I don't think I know you"
+      end
+    end
   end
 
   def logout
