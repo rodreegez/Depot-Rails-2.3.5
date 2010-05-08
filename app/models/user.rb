64ed46688524c4d1356569612b8375eeece6e445
Adam <electronicbattleweapon7@gmail.com>
2010-05-08 11:51:09 +0100

    create a virtual attribute for password that is not persisted in the db - returns the salted hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,17 @@ class User < ActiveRecord::Base
 
   validate :password_non_blank
 
+  def password
+    @password
+  end
+
+  def password=(pwd)
+    @password = pwd
+    return if pwd.blank?
+    create_new_salt
+    self.hashed_password = User.encrypted_password(self.password, self.salt)
+  end
+
   private
 
   def self.encrypted_password(passowrd, salt)
@@ -20,4 +31,9 @@ class User < ActiveRecord::Base
   def password_non_blank
     errors.add(:password, "Missing password") if hashed_password.blank?
   end
+
+  def create_new_salt
+    self.salt = self.object_id.to_s + rand.to_s
+  end
+
 end
