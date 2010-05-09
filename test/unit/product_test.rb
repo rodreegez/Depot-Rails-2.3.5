5e7d373f69b7482116f1461c406b39dad761d430
Adam <electronicbattleweapon7@gmail.com>
2010-05-09 21:26:00 +0100

    add product validations test

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test 'invalid if attrs are empty' do
+    product = Product.new
+    assert !product.valid?
+    assert product.errors.invalid?(:title)
+    assert product.errors.invalid?(:description)
+    assert product.errors.invalid?(:price)
+    assert product.errors.invalid?(:image_url)
   end
 end
