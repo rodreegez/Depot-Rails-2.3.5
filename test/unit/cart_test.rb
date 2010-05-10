c8a1466a50ba0cbab90f8408bb09673fb0465ee8
Adam <electronicbattleweapon7@gmail.com>
2010-05-10 10:24:21 +0100

    test adding to cart

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :products
+
+  test "add unique products" do
+    c = Cart.new
+    ra = products(:rails_book)
+    ru = products(:ruby_book)
+    c.add_product ra
+    c.add_product ru
+    assert_equal 2, c.items.size
+    assert_equal ra.price + ru.price, c.total_price
   end
 end
