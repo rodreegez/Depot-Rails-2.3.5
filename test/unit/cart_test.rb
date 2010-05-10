335e38c0819177c0f0bed728f842d3e1bd92ce49
Adam <electronicbattleweapon7@gmail.com>
2010-05-10 19:47:33 +0100

    add setup method to creat each book and a cart

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -2,25 +2,26 @@ require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
   fixtures :products
+  
+  def setup 
+    @cart = Cart.new 
+    @rails = products(:rails_book) 
+    @ruby = products(:ruby_book)
+  end
 
   test "add unique products" do
-    c = Cart.new
-    ra = products(:rails_book)
-    ru = products(:ruby_book)
-    c.add_product ra
-    c.add_product ru
-    assert_equal 2, c.items.size
-    assert_equal ra.price + ru.price, c.total_price
+    @cart.add_product @rails
+    @cart.add_product @ruby
+    assert_equal 2, @cart.items.size
+    assert_equal @rails.price + @ruby.price, @cart.total_price
   end
 
   test "add duplicate product" do
-    cart = Cart.new
-    ra = products(:rails_book)
-    cart.add_product ra
-    cart.add_product ra
-    assert_equal (2 * ra.price), cart.total_price
-    assert_equal 1, cart.items.size
-    assert_equal 2, cart.items[0].quantity
+    @cart.add_product @rails
+    @cart.add_product @rails
+    assert_equal (2 * @rails.price), @cart.total_price
+    assert_equal 1, @cart.items.size
+    assert_equal 2, @cart.items[0].quantity
   end
 
 end
