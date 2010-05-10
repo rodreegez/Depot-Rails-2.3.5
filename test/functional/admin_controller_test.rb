7cd39b52b0727228ef83ecda5294616bd39363ee
Adam <electronicbattleweapon7@gmail.com>
2010-05-10 22:19:53 +0100

    add dynamic fixture for user, test getting the index w/ user, test login

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "index" do
+    get :index
+    assert_redirected_to :action => "login"
+    assert_equal "Please log in", flash[:notice]
+  end
+
+  test "index with user" do
+    get :index, {}, { :user_id => users(:Adam).id }
+    assert_response :success
+    assert_template "index"
+  end
+
+  test "login" do
+    me = users(:Adam)
+    post :login, :name => me.name, :password => 'secret'
+    assert_redirected_to :action => "index"
+    assert_equal me.id, session[:user_id]
   end
 end
